e4f9a3ca3082bfbd9d3434fb59fae678a0515b00
binarioGH <lacuentafalsadediegojonas@gmail.com>
2020-01-25 00:25:55 -0800

    counter

--- main.py
+++ main.py
@@ -8,14 +8,24 @@ def playsound(file):
 	PlaySound(file, SND_ASYNC)
 
 class Pier:
-	def __init__(self, pi, root):
+	def __init__(self, pi, root, feed, counter):
 		self.pi = pi
+		self.feed = feed
 		self.pindex = 0
 		self.fontsize = 20
 		self.root = root
+		self.counter = counter
+		self.counter["text"] = "Counter: {}".format(self.pindex)
 
 	def right(self, entry):
 		number = entry.get()
+		if not len(number):
+			return 0
+		else:
+			number = number[0]
+			if number == ",":
+				number = "."
+
 		entry.delete(0, 'end')
 		if number == PI[self.pindex]:
 			self.pi["text"] = self.pi["text"] + PI[self.pindex]
@@ -31,6 +41,7 @@ class Pier:
 			self.pi.config(bg="#FF9760")
 			
 		else:
+			self.feed["text"] = "Last number: {}".format(PI[self.pindex])
 			self.pindex = 0
 			self.pi["text"] = ""
 			self.fontsize = 20
@@ -41,6 +52,7 @@ class Pier:
 			self.root.update()
 			sleep(0.1)
 			self.pi.config(bg="#FF9760")
+		self.counter["text"] = "Counter: {}".format(self.pindex)
 
 
 def main():
@@ -53,7 +65,11 @@ def main():
 	title.place(relx=0.3, rely=0.1, relwidth=0.4, relheight=0.1)
 	pi = Label(mframe, bg="#FF9760", font=("Courier", 20), text="")
 	pi.place(relx=0, rely=0.3, relwidth=1, relheight=0.05)
-	pier = Pier(pi, root)
+	feed = Label(mframe, bg="white", font=("Courier", 20), fg="#FF1C0F")
+	feed.place(relx=0.35, rely=0.75, relwidth=0.35, relheight=0.1)
+	counter = Label(mframe, bg="white", font=("Courier", 20), fg="#FF1C0F")
+	counter.place(relx=0.35,rely=0.6, relwidth=0.35, relheight=0.1)
+	pier = Pier(pi, root, feed, counter)
 	digit = Entry(mframe, borderwidth=2, relief="solid", font=("Courier", 18))
 	digit.place(relx=0.35, rely=0.5, relwidth=0.15, relheight=0.1)
 	check = Button(mframe, bg="#FF9760", fg="black", font='Helvetica 18',text="Check!", command=lambda:pier.right(digit))
