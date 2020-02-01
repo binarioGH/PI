5f561276b8c0ed560aa347da15196f512d1cc6e6
binarioGH <lacuentafalsadediegojonas@gmail.com>
2020-01-31 23:30:58 -0800

    i got this pi generating algorithm

---
+++ source/generate_pi.py
@@ -0,0 +1,31 @@
+#-*-coding: utf-8-*-
+#https://www.w3resource.com/projects/python/python-projects-1.php
+
+def calcPi(limit):
+    q, r, t, k, n, l = 1, 0, 1, 1, 3, 3
+
+    decimal = limit
+    counter = 0
+
+    while counter != decimal + 1:
+            if 4 * q + r - t < n * t:
+                    yield n
+                    if counter == 0:
+                            yield '.'
+                    if decimal == counter:
+                            print('')
+                            break
+                    counter += 1
+                    nr = 10 * (r - n * t)
+                    n = ((10 * (3 * q + r)) // t) - 10 * n
+                    q *= 10
+                    r = nr
+            else:
+                    nr = (2 * q + r) * l
+                    nn = (q * (7 * k) + 2 + (r * l)) // (t * l)
+                    q *= k
+                    t *= l
+                    l += 2
+                    k += 1
+                    n = nn
+                    r = nr
